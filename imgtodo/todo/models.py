968d581614aa72f1fa306402ba44566c17521edc
KeitoH <ht20a082@oecu.jp>
2022-09-11 13:23:19 +0900

    4-5 create model

--- imgtodo/todo/models.py
+++ imgtodo/todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+CATEGORY = (('school', '学校'), ('work', '仕事'), ('hoby', '趣味'), ('private', 'プライベート'))
+
+class Todo(models.Model):
+  title = models.CharField(max_length=100)
+  body = models.TextField()
+  category = models.CharField(
+    max_length=100,
+    choices = CATEGORY
+  )
+
+  def __str__(self):
+    return self.title
--- imgtodo/todo/views.py
+++ imgtodo/todo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Todo
 
-# Create your views here.
+class ListTodoView(ListView):
+  template_name = 'todo/todo_list.html'
+  model = Todo

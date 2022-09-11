eb74a059576caef18b9cc89aee94040eb1ca418d
KeitoH <ht20a082@oecu.jp>
2022-09-11 17:59:12 +0900

    4-9 create create

--- imgtodo/todo/urls.py
+++ imgtodo/todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('todo/', views.ListTodoView.as_view()),
+  path('todo/', views.ListTodoView.as_view(), name='list-todo'),
+  path('todo/create/', views.CreateTodoView.as_view(), name='create-todo'),
 ]
--- imgtodo/todo/views.py
+++ imgtodo/todo/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 from .models import Todo
 
 class ListTodoView(ListView):
   template_name = 'todo/todo_list.html'
   model = Todo
+
+class CreateTodoView(CreateView):
+  template_name = 'todo/todo_create.html'
+  model = Todo
+  fields = ('title', 'body', 'category')
+  success_url = reverse_lazy('list-todo')
